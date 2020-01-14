1c3a2b9a975c75fc3aae2a17bdd36e8a5a0f1476
rokinsky <ankezy@gmail.com>
2020-01-14 23:09:58 +0100

    crypto: two hashes works

--- crypto/srv_osx.py
+++ crypto/srv_osx.py
@@ -2,9 +2,12 @@ import Crypto.Cipher.AES as AES
 from os import urandom, listdir
 import sys
 
-DEBUG = False
+DEBUG = True
 
-key = urandom(16)
+key = urandom(16) # b'\x00' * 16
+
+if DEBUG:
+    sys.stderr.write('\n\n\n\n\n' + 'KEY ' + repr(key) + '\n')
 
 def enc(msg):
     if DEBUG:
@@ -13,7 +16,7 @@ def enc(msg):
     # oracle attacks — it's much more secure to just pad with spaces.
     while len(msg) % 16 != 0:
         msg += b' '
-    iv = urandom(16)
+    iv = getrandom(16)
     cipher = AES.new(mode=AES.MODE_CBC, key=key, iv=iv)
     return iv + cipher.encrypt(msg)
 
@@ -24,7 +27,10 @@ def dec(msg):
     return cipher.decrypt(msg)
 
 print("Welcome to the encrypted shell!")
+i = 0
+
 while True:
+    #sys.stderr.write("Enter encrypted command." + '\n') 
     print("Enter encrypted command.")
     # Get a line.
     try:
@@ -34,19 +40,26 @@ while True:
     try:
         # Decode it.
         d = bytes.fromhex(d.strip())
+        #sys.stderr.write(f'MSG {d}' + '\n')
 
         # Decrypt it.
         d = dec(d)
 
         # Leave the message as bytes — Unicode is annoying and just leads to trouble.
-        if DEBUG:
-            sys.stderr.write('DEC ' + repr(d) + '\n')
+        #if DEBUG:
+            #sys.stderr.write('DEC ' + repr(d) + '\n')
 
         # Strip comments.
         if b'#' in d:
+            #i = 0
+            sys.stderr.write('#=' + repr(i) + ' ' + repr(d) + '\n')
             pos = d.index(b'#')
             d = d[:pos]
 
+        #if b' ' in d[:1]:
+        #    sys.stderr.write('S=' + repr(i) + ' ' + repr(d) + '\n')
+
+        i = (i + 1) % 255
         # Split by spaces.
         words = []
         word = b''
@@ -57,11 +70,19 @@ while True:
                     word = b''
             else:
                 word += bytes([x])
+
+            #if DEBUG:
+            #    sys.stderr.write('WORD ' + repr(word) + '\n')
         if word:
             words.append(word)
+        #words = filter(lambda x: x != b'', d.split(b' '))
+
+        #sys.stderr.write(repr(words[0]) + " " + repr(words[1]) + '\n')
 
         if not words:
             # Empty command?
+            #print('GOOD')
+            sys.stderr.write('EMPTY' + '\n')
             continue
 
         if words[0] == b'ls':
@@ -79,7 +100,8 @@ while True:
                     except OSError:
                         print(enc(f + b': no such file or directory').hex())
         else:
-            print(enc(words[0] + b': unknown command').hex())
+            print(b'\n')
+            #sys.stderr.write(repr(enc(words[0] + b': unknown command')) + '\n')
     except Exception as e:
         print(enc(f'Exception occured: {e}'.encode()).hex())
 
